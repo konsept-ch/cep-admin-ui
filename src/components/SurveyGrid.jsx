5bb796990ed2bcffea5899c08525c582e8eaad0e
Yana <yansy.blue.1998@gmail.com>
2020-09-09 15:55:15 +0300

    remove claroline fetch on component mount

--- src/components/SurveyGrid.jsx
+++ src/components/SurveyGrid.jsx
@@ -4,7 +4,6 @@ import 'ag-grid-enterprise'
 import surveyDataMock from './../mock/survey-data'
 
 import React, { useState, useEffect } from 'react'
-import base64 from 'base-64'
 import { AgGridReact } from 'ag-grid-react'
 import { ModuleRegistry, AllModules } from '@ag-grid-enterprise/all-modules'
 ModuleRegistry.registerModules(AllModules)
@@ -22,19 +21,7 @@ function SurveyGrid() {
     ])
     const [rowData, setRowData] = useState(null)
 
-    useEffect(() => {
-        fetch('https://cep-dev.ch/apiv2/workspace/3', {
-            method: 'GET',
-            mode: 'no-cors',
-            credentials: 'include',
-            headers: new Headers({
-                'X-Auth-Token': 'c1b3fae80065e32fbcaf1bf0beae3392848a',
-                Cookie: 'PHPSESSID=71f4632f847479593c50fd6c81c4dace',
-                Authorization: 'Basic ' + base64.encode('user:abc123'),
-            }),
-        }).then(console.log)
-        setRowData(surveyDataMock)
-    }, [])
+    useEffect(() => setRowData(surveyDataMock), [])
 
     return (
         <div>
