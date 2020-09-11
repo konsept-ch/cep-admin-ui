d8cff624692f56d7dd90bef61b090f058f490b11
Yana <yansy.blue.1998@gmail.com>
2020-09-11 17:10:12 +0300

    add eslint configuration regarding prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends: ['react-app'],
+    extends: ['react-app', 'plugin:prettier/recommended', 'prettier/react'],
     rules: {
         'no-console': 'error',
         'no-unused-vars': 'error',
