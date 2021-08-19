c1008d5b0c9efb7d88966a140212466afe204bfa
Yana Ivanova <yana@plam.ch>
2021-08-19 13:36:35 +0300

    add agenda page - wip

--- src/components/AgendaPage.jsx
+++ src/components/AgendaPage.jsx
@@ -1,39 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Offcanvas } from 'react-bootstrap'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid' // a plugin!
 import { agendaEvents } from '../mock/agenda-events' // a plugin!
 
-export function AgendaPage() {
+export const AgendaPage = React.memo(() => {
+    const [selectedEvent, setSelectedEvent] = useState(null)
+
+    const dateOptions = {
+        year: 'numeric',
+        month: 'long',
+        weekday: 'long',
+        hour: 'numeric',
+        minute: 'numeric',
+    }
+
     return (
-        <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin]}
-            headerToolbar={{
-                left: 'prev,next today',
-                center: 'title',
-                right: 'timeGridWeek,timeGridDay,timeGridFourDay',
-            }}
-            views={{
-                timeGridFourDay: {
-                    type: 'timeGrid',
-                    duration: { days: 4 },
-                    buttonText: '4 day',
-                },
-            }}
-            timeZone="UTC"
-            initialView="timeGridWeek"
-            events={agendaEvents}
-            eventContent={(eventInfo) => (
-                <>
-                    {eventInfo.timeText && (
-                        <>
-                            <b>{eventInfo.timeText}</b>
-                            <br />
-                        </>
-                    )}
-                    <i>{eventInfo.event.title}</i>
-                </>
+        <Container fluid className="my-3">
+            <FullCalendar
+                eventClick={function (info) {
+                    info.jsEvent.preventDefault()
+                    setSelectedEvent({ ...info.event._def, range: info.event._instance.range })
+                }}
+                height="65vh"
+                plugins={[dayGridPlugin, timeGridPlugin]}
+                headerToolbar={{
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'timeGridWeek,timeGridDay,timeGridFourDay',
+                }}
+                views={{
+                    timeGridFourDay: {
+                        type: 'timeGrid',
+                        duration: { days: 4 },
+                        buttonText: '4 day',
+                    },
+                }}
+                timeZone="UTC"
+                initialView="timeGridWeek"
+                events={agendaEvents}
+                eventContent={(eventInfo) => (
+                    <>
+                        {eventInfo.timeText && (
+                            <>
+                                <b>{eventInfo.timeText}</b>
+                                <br />
+                            </>
+                        )}
+                        <i>{eventInfo.event.title}</i>
+                    </>
+                )}
+            />
+            {selectedEvent && (
+                <Offcanvas
+                    show={selectedEvent !== null}
+                    onHide={() => setSelectedEvent(null)}
+                    backdropClassName="offcanvas-backdrop"
+                >
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title>{selectedEvent.title}</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <dl>
+                            <dt>Desctiprion</dt>
+                            <dd>{selectedEvent.extendedProps.description}</dd>
+
+                            <dt>Room</dt>
+                            <dd>{selectedEvent.extendedProps.room}</dd>
+
+                            <dt>Time</dt>
+                            <dd className="pl-3">
+                                <b>Start</b> -{' '}
+                                {Intl.DateTimeFormat('fr-CH', dateOptions).format(selectedEvent.range.start)}
+                                <br />
+                                <b>End</b> - {Intl.DateTimeFormat('fr-CH', dateOptions).format(selectedEvent.range.end)}
+                            </dd>
+                        </dl>
+                    </Offcanvas.Body>
+                </Offcanvas>
             )}
-        />
+        </Container>
     )
-}
+})
+
+AgendaPage.displayName = 'AgendaPage'
--- src/mock/agenda-events.js
+++ src/mock/agenda-events.js
@@ -1,11 +1,13 @@
 export const agendaEvents = [
     {
         title: 'All Day Event',
+        room: 'Classroom',
         description: 'description for All Day Event',
         start: '2021-08-15',
     },
     {
         title: 'Long Event',
+        room: 'Classroom',
         description: 'description for Long Event',
         start: '2021-08-15',
         end: '2021-08-18',
@@ -13,44 +15,52 @@ export const agendaEvents = [
     {
         groupId: '999',
         title: 'Repeating Event',
+        room: 'Classroom',
         description: 'description for Repeating Event',
         start: '2021-08-16T16:00:00',
     },
     {
         groupId: '999',
         title: 'Repeating Event',
+        room: 'Classroom',
         description: 'description for Repeating Event',
         start: '2021-08-17T16:00:00',
     },
     {
         title: 'Conference',
+        room: 'Classroom',
         description: 'description for Conference',
         start: '2021-08-20',
         end: '2021-08-21',
     },
     {
         title: 'Meeting',
+        room: 'Classroom',
         description: 'description for Meeting',
         start: '2021-08-19T10:30:00',
         end: '2021-08-19T12:30:00',
     },
     {
         title: 'Lunch',
+        room: 'Classroom',
         description: 'description for Lunch',
         start: '2021-08-18T12:00:00',
     },
     {
         title: 'Meeting',
+        room: 'Classroom',
         description: 'description for Meeting',
         start: '2021-08-18T14:30:00',
     },
     {
         title: 'Birthday Party',
+        room: 'Classroom',
         description: 'description for Birthday Party',
         start: '2021-08-16T07:00:00',
     },
     {
         title: 'Click for Google',
+        room: 'Classroom',
         description: 'description for Click for Google',
         url: 'http://google.com/',
         start: '2021-08-20',
