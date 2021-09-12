1f7faf660b8e0700301c6816f21b8febaaf6ca1a
Daniel Rodriguez <maieutiquer@gmail.com>
2021-09-12 22:44:29 +0300

    add action on status change

--- src/pages/InscriptionsPage.jsx
+++ src/pages/InscriptionsPage.jsx
@@ -43,13 +43,29 @@ export function InscriptionsPage() {
                     'Écartée',
                 ],
             },
+            onCellValueChanged: ({ data: { inscriptionId }, newValue }) => {
+                // console.log(node)
+                ;(async () => {
+                    const inscriptionsResponse = await fetch(`http://localhost:4000/inscriptions/${inscriptionId}`, {
+                        method: 'post',
+                        mode: 'no-cors',
+                        body: JSON.stringify({ status: newValue }),
+                    })
+
+                    console.log(inscriptionsResponse)
+                    const json = await inscriptionsResponse.json()
+
+                    console.log(json)
+                })()
+            },
         },
         { field: 'startDate', headerName: 'Date de début', filter: 'agDateColumnFilter' },
     ]
 
-    // console.log('inscriptions', inscriptions)
+    console.log('inscriptions', inscriptions)
 
-    const rowData = inscriptions?.map(({ user, session, validated, confirmed, date }) => ({
+    const rowData = inscriptions?.map(({ id: inscriptionId, user, session, validated, confirmed, date }) => ({
+        inscriptionId,
         participant: user.name,
         profession: '(à faire)',
         session: session.name,
