7fe6c5fa8f954a402048164aef5ec846696aa47b
Victor Getsov <viktorgetsov@gmail.com>
2021-09-22 02:11:28 +0300

    add sticky footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,37 @@ import { SessionsPage } from './pages/SessionsPage'
 import { AgendaPage } from './pages/AgendaPage'
 import { TypographyPage } from './pages/TypographyPage'
 import { Navigation } from './components'
+import { Footer } from './components/Footer'
 import { PATH_INSCRIPTIONS, PATH_SESSIONS, PATH_AGENDA } from './constants/constants'
 
 export function App() {
     return (
-        <HelmetProvider>
-            <Helmet>
-                <title>CEP - Former22</title>
-            </Helmet>
-            <Navigation />
-            <Switch>
-                <Redirect exact from="/" to={PATH_INSCRIPTIONS} />
-                <Route exact path={PATH_AGENDA}>
-                    <AgendaPage />
-                </Route>
-                <Route exact path={PATH_INSCRIPTIONS}>
-                    <InscriptionsPage />
-                </Route>
-                <Route exact path={PATH_SESSIONS}>
-                    <SessionsPage />
-                </Route>
-                <Route path="/survey/">
-                    <SurveyPage />
-                </Route>
-                <Route path="/typography">
-                    <TypographyPage />
-                </Route>
-            </Switch>
-        </HelmetProvider>
+        <>
+            <HelmetProvider>
+                <Helmet>
+                    <title>CEP - Former22</title>
+                </Helmet>
+                <Navigation />
+                <Switch>
+                    <Redirect exact from="/" to={PATH_INSCRIPTIONS} />
+                    <Route exact path={PATH_AGENDA}>
+                        <AgendaPage />
+                    </Route>
+                    <Route exact path={PATH_INSCRIPTIONS}>
+                        <InscriptionsPage />
+                    </Route>
+                    <Route exact path={PATH_SESSIONS}>
+                        <SessionsPage />
+                    </Route>
+                    <Route path="/survey/">
+                        <SurveyPage />
+                    </Route>
+                    <Route path="/typography">
+                        <TypographyPage />
+                    </Route>
+                </Switch>
+            </HelmetProvider>
+            <Footer />
+        </>
     )
 }
