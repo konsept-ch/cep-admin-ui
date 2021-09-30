816b6189a366dbbe9d093f940ebf3e45736cf503
Victor Getsov <viktorgetsov@gmail.com>
2021-09-30 23:37:19 +0300

    move room selection to the right

--- src/pages/AgendaPage.jsx
+++ src/pages/AgendaPage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Container, Form, Button } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye, faEyeSlash } from '@fortawesome/pro-solid-svg-icons'
+import { faEye, faEyeSlash, faArrowLeftFromLine, faArrowRightToLine } from '@fortawesome/pro-solid-svg-icons'
 
 import { Calendar } from '../components'
 import { fetchAgendaAction } from '../actions/agenda.ts'
@@ -14,6 +14,7 @@ export const AgendaPage = () => {
     const { rooms, events } = useSelector(roomsAndEventsSelector)
     const [selectedRooms, setSelectedRooms] = useState(markAllRoomsAsSelected({ rooms }))
     const dispatch = useDispatch()
+    const [isRoomSelectionExpanded, setRoomSelectionExpanded] = useState(false)
 
     useEffect(() => {
         dispatch(fetchAgendaAction())
@@ -62,70 +63,90 @@ export const AgendaPage = () => {
             setSelectedRooms({ ...currentlySelectedRooms, [id]: target.checked })
 
     return (
-        <Container fluid className="my-3">
-            <Calendar
-                resources={rooms.filter(({ id }) => selectedRooms[id])}
-                events={events.filter(({ room: { id } }) => selectedRooms[id])}
-            />
-            <div className="room-selection">
-                {rooms.map(({ name, id }) => (
-                    <Form.Check
-                        inline
-                        type="checkbox"
-                        checked={selectedRooms[id] === true}
-                        id={id}
-                        key={id}
-                        label={
-                            <>
-                                {selectedRooms[id] ? (
-                                    <FontAwesomeIcon icon={faEye} />
-                                ) : (
-                                    <FontAwesomeIcon icon={faEyeSlash} />
-                                )}{' '}
-                                {name}
-                            </>
-                        }
-                        onChange={onRoomCheckboxClick({ currentlySelectedRooms: selectedRooms, id })}
-                    />
-                ))}
+        <div className="calendar-page mt-3">
+            <Container fluid>
+                <Calendar
+                    resources={rooms.filter(({ id }) => selectedRooms[id])}
+                    events={events.filter(({ room: { id } }) => selectedRooms[id])}
+                />
+            </Container>
+            <div className="d-flex">
+                <div className={`room-selection collapse collapse-horizontal ${isRoomSelectionExpanded ? 'show' : ''}`}>
+                    <div className="card card-body">
+                        {rooms.map(({ name, id }) => (
+                            <Form.Check
+                                type="checkbox"
+                                checked={selectedRooms[id] === true}
+                                id={id}
+                                key={id}
+                                label={
+                                    <>
+                                        {selectedRooms[id] ? (
+                                            <FontAwesomeIcon icon={faEye} />
+                                        ) : (
+                                            <FontAwesomeIcon icon={faEyeSlash} />
+                                        )}{' '}
+                                        {name}
+                                    </>
+                                }
+                                onChange={onRoomCheckboxClick({ currentlySelectedRooms: selectedRooms, id })}
+                            />
+                        ))}
+                        <div className="bulk-selection">
+                            <Button
+                                variant="outline-primary"
+                                onClick={selectAllRooms}
+                                active={rooms.every(({ id }) => selectedRooms[id] === true)}
+                            >
+                                Toutes salles ({rooms.length})
+                            </Button>{' '}
+                            <Button
+                                variant="outline-primary"
+                                onClick={unselectAllRooms}
+                                active={!Object.values(selectedRooms).includes(true)}
+                            >
+                                Aucune
+                            </Button>{' '}
+                            <Button variant="outline-primary" onClick={inverseAllRooms}>
+                                Inverse
+                            </Button>{' '}
+                            <Button
+                                variant="outline-primary"
+                                onClick={selectZoomRooms}
+                                active={rooms
+                                    .filter(({ capacity }) => capacity === 0)
+                                    .every(({ id }) => selectedRooms[id] === true)}
+                            >
+                                + Zoom
+                            </Button>{' '}
+                            <Button
+                                variant="outline-primary"
+                                onClick={removeZoomRooms}
+                                active={rooms
+                                    .filter(({ capacity }) => capacity === 0)
+                                    .every(({ id }) => selectedRooms[id] === false)}
+                            >
+                                - Zoom
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+                <button
+                    className="toggle-room-selection btn btn-outline-primary"
+                    type="button"
+                    onClick={() => {
+                        setRoomSelectionExpanded(!isRoomSelectionExpanded) // при клик извикваме set с обратното на is
+                    }}
+                    aria-expanded="false"
+                    aria-controls="collapseExample"
+                >
+                    {isRoomSelectionExpanded ? (
+                        <FontAwesomeIcon icon={faArrowRightToLine} />
+                    ) : (
+                        <FontAwesomeIcon icon={faArrowLeftFromLine} />
+                    )}
+                </button>
             </div>
-            <div className="bulk-selection">
-                <Button
-                    variant="outline-primary"
-                    onClick={selectAllRooms}
-                    active={rooms.every(({ id }) => selectedRooms[id] === true)}
-                >
-                    Toutes salles ({rooms.length})
-                </Button>{' '}
-                <Button
-                    variant="outline-primary"
-                    onClick={unselectAllRooms}
-                    active={!Object.values(selectedRooms).includes(true)}
-                >
-                    Aucune
-                </Button>{' '}
-                <Button variant="outline-primary" onClick={inverseAllRooms}>
-                    Inverse
-                </Button>{' '}
-                <Button
-                    variant="outline-primary"
-                    onClick={selectZoomRooms}
-                    active={rooms
-                        .filter(({ capacity }) => capacity === 0)
-                        .every(({ id }) => selectedRooms[id] === true)}
-                >
-                    + Zoom
-                </Button>{' '}
-                <Button
-                    variant="outline-primary"
-                    onClick={removeZoomRooms}
-                    active={rooms
-                        .filter(({ capacity }) => capacity === 0)
-                        .every(({ id }) => selectedRooms[id] === false)}
-                >
-                    - Zoom
-                </Button>
-            </div>
-        </Container>
+        </div>
     )
 }
