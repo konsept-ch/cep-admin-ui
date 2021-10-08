fc9f829cf965276821b8f726a3f1cbca0d1757b4
Daniel Rodriguez <maieutiquer@gmail.com>
2021-10-09 02:23:52 +0300

    course program - add and remove events

--- src/components/CourseDetailsModal.jsx
+++ src/components/CourseDetailsModal.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import { Modal, Button, Form, FloatingLabel, Row, Col } from 'react-bootstrap'
 
 export const CourseDetailsModal = ({ closeModal, courseDetailsData }) => {
-    const [eventFields, setEventFields] = useState({ type: 'f2f', title: 'test title', description: 'test desc' })
-
+    const defaultEvent = { type: 'f2f', title: 'test title', description: 'test desc' }
+    const [events, setEvents] = useState([defaultEvent])
     const onChangeEventField =
-        ({ fieldId }) =>
+        ({ fieldName, eventOrder }) =>
         ({ target: { value } }) =>
-            setEventFields((fields) => ({ ...fields, [fieldId]: value }))
+            setEvents((previousEvents) =>
+                previousEvents.map((previousEvent, previousEventOrder) =>
+                    previousEventOrder === eventOrder ? { ...previousEvent, [fieldName]: value } : previousEvent
+                )
+            )
 
     return (
         <Modal show onHide={closeModal} backdrop="static" keyboard={false} dialogClassName="course-details-modal">
@@ -18,39 +22,72 @@ export const CourseDetailsModal = ({ closeModal, courseDetailsData }) => {
                 <Row>
                     <Col sm={4}>
                         <h4>Construction du déroulement</h4>
-                        <Form>
-                            <FloatingLabel controlId="type" label="Type de séance">
-                                <Form.Select
-                                    aria-label="Type de séance"
-                                    onChange={onChangeEventField({ fieldId: 'type' })}
-                                >
-                                    <option value="f2f">En salle</option>
-                                    <option value="sync">En ligne - Synchronne (en salle virtuelle)</option>
-                                    <option value="async">En ligne - Asynchronne (à son rythme)</option>
-                                </Form.Select>
-                            </FloatingLabel>
-                            <FloatingLabel controlId="title" label="Titre">
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Titre de la séance"
-                                    onChange={onChangeEventField({ fieldId: 'title' })}
-                                />
-                            </FloatingLabel>
-                            <FloatingLabel controlId="description" label="Description">
-                                <Form.Control
-                                    as="textarea"
-                                    placeholder="Description de la séance"
-                                    style={{ height: '200px' }}
-                                    onChange={onChangeEventField({ fieldId: 'description' })}
-                                />
-                            </FloatingLabel>
-                        </Form>
+                        {events.map(({ type, title, description }, eventOrder) => (
+                            <Form key={title} className="mb-4">
+                                <Row>
+                                    <Col sm={2}>
+                                        <Button
+                                            variant="danger"
+                                            onClick={() =>
+                                                setEvents((previousEvents) =>
+                                                    previousEvents.filter(
+                                                        (_, previousEventOrder) => eventOrder !== previousEventOrder
+                                                    )
+                                                )
+                                            }
+                                        >
+                                            Effacer
+                                        </Button>
+                                    </Col>
+                                    <Col sm={10}>
+                                        <FloatingLabel controlId="title" label="Titre" className="mb-3">
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="Titre de la séance"
+                                                value={title}
+                                                onChange={onChangeEventField({ fieldName: 'title', eventOrder })}
+                                            />
+                                        </FloatingLabel>
+                                        <FloatingLabel controlId="description" label="Description" className="mb-3">
+                                            <Form.Control
+                                                as="textarea"
+                                                placeholder="Description de la séance"
+                                                style={{ height: '200px' }}
+                                                value={description}
+                                                onChange={onChangeEventField({ fieldName: 'description', eventOrder })}
+                                            />
+                                        </FloatingLabel>
+                                        <FloatingLabel controlId="type" label="Type de séance" className="mb-3">
+                                            <Form.Select
+                                                aria-label="Type de séance"
+                                                value={type}
+                                                onChange={onChangeEventField({ fieldName: 'type', eventOrder })}
+                                            >
+                                                <option value="f2f">En salle</option>
+                                                <option value="sync">En ligne - Synchronne (en salle virtuelle)</option>
+                                                <option value="async">En ligne - Asynchronne (à son rythme)</option>
+                                            </Form.Select>
+                                        </FloatingLabel>
+                                    </Col>
+                                </Row>
+                            </Form>
+                        ))}
+                        <Button
+                            variant="primary"
+                            onClick={() => setEvents((previousEvents) => [...previousEvents, defaultEvent])}
+                        >
+                            Ajouter
+                        </Button>
                     </Col>
                     <Col sm={8}>
                         <h4>Prévisualisation de la description</h4>
-                        <p>{eventFields.type}</p>
-                        <p>{eventFields.title}</p>
-                        <p>{eventFields.description}</p>
+                        {events.map(({ type, title, description }) => (
+                            <div>
+                                <p>{type}</p>
+                                <p>{title}</p>
+                                <p>{description}</p>
+                            </div>
+                        ))}
                     </Col>
                 </Row>
             </Modal.Body>
