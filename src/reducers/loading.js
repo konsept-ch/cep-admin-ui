fa5a5db76fe72c7fa36995f0e40903e7ac103b7b
Yana Ivanova <yana@plam.ch>
2021-10-19 17:17:05 +0300

    add loader in grid and set grid data as immutable

--- src/reducers/loading.js
+++ src/reducers/loading.js
@@ -1,14 +1,21 @@
-import { SET_LOADING } from '../constants/loading'
+import { SET_LOADING, SET_GRID_LOADING } from '../constants/loading'
 
 const initialState = {
     loading: false,
+    gridLoading: false,
 }
 
 export const loadingReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_LOADING:
             return {
+                ...state,
                 loading: action.payload.loading,
+            }
+        case SET_GRID_LOADING:
+            return {
+                ...state,
+                gridLoading: action.payload.loading,
             }
         default:
             return state
@@ -17,4 +24,5 @@ export const loadingReducer = (state = initialState, action) => {
 
 export const loadingSelectors = {
     loadingSelector: (state) => state.loading,
+    gridLoadingSelector: (state) => state.gridLoading,
 }
