b228723437e66391d4e54676106dcd68d94af81a
Daniel Rodriguez <dani@plam.ch>
2022-08-09 11:08:41 +0300

    add more fields

--- src/components/ManualInvoiceModal.jsx
+++ src/components/ManualInvoiceModal.jsx
@@ -1,37 +1,60 @@
 import { useEffect, useState } from 'react'
 import { Button, Spinner, Row, Form, Col, OverlayTrigger, Tooltip } from 'react-bootstrap'
-import { useForm } from 'react-hook-form'
+import { useForm, useFieldArray } from 'react-hook-form'
 import { toast } from 'react-toastify'
+import Select from 'react-select'
 
 import { CommonModal } from '../components'
 import { useUpdateManualInvoiceMutation } from '../services/invoices'
 import { useGetOrganizationsFlatWithAddressQuery } from '../services/organizations'
 import { formatToFlatObject } from '../utils'
 
+const getInvoiceNumber = ({ courseYear, userCode, invoiceNumberForCurrentYear }) =>
+    ` ${`${courseYear}`?.slice(-2)}${`${userCode}`?.padStart(2, 0)}${`${invoiceNumberForCurrentYear}`?.padStart(4, 0)}`
+
 export function ManualInvoiceModal({ refetchInvoices, selectedInvoiceData, closeModal, isModalOpen }) {
     const {
+        control,
         register,
         handleSubmit,
         reset,
         formState: { isDirty },
+        watch,
     } = useForm()
+
+    const { fields } = useFieldArray({ control, name: 'items' })
+
+    const courseYearWatched = watch('courseYear')
 
     const { data: organizations, refetch: refetchOrganizations } = useGetOrganizationsFlatWithAddressQuery(null, {
         refetchOnMountOrArgChange: true,
     })
 
-    console.log(organizations)
+    // console.log(organizations)
 
     useEffect(() => {
         if (selectedInvoiceData != null) {
-            const { invoiceReason } = selectedInvoiceData
+            const { customClientAddress, invoiceReason, courseYear, invoiceDate, vatCode, items, customClientEmail } =
+                selectedInvoiceData
 
             reset({
+                customClientAddress,
                 invoiceReason,
+                courseYear,
+                invoiceDate,
+                vatCode,
+                items,
+                customClientEmail,
             })
         } else {
             reset({
+                customClientAddress: '',
+                customClientEmail: '',
                 invoiceReason: '',
+                courseYear: '',
+                invoiceDate: '',
+                vatCode: '',
+                items: [],
             })
         }
     }, [selectedInvoiceData])
@@ -56,16 +79,83 @@ export function ManualInvoiceModal({ refetchInvoices, selectedInvoiceData, close
             <CommonModal
                 title="Ajouter une facture"
                 content={
-                    <Row>
-                        <Col>
-                            <h6>Détails de la facture</h6>
-                        </Col>
-                        <Col>
-                            <h6>Modifier la facture</h6>
-                            <Form.Label>Nom du participant</Form.Label>
-                            <Form.Control as="textarea" {...register('invoiceReason')} />
-                        </Col>
-                    </Row>
+                    <>
+                        <Row>
+                            <Col>
+                                <h6>Modifier la facture</h6>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col />
+                            <Col>
+                                <Form.Label>Client</Form.Label>
+                                <Select
+                                    options={organizations?.map(({ code, name }) => ({
+                                        value: code,
+                                        label: `${code} - ${name}`,
+                                    }))}
+                                    isClearable
+                                />
+                                <Form.Label>Adresse postale</Form.Label>
+                                <Form.Control as="textarea" {...register('customClientAddress')} />
+                                <Form.Label>E-mail</Form.Label>
+                                <Form.Control {...register('customClientEmail')} />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Form.Label>Concerne</Form.Label>
+                                <Form.Control {...register('invoiceReason')} />
+                            </Col>
+                            <Col />
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Form.Label>Année formation</Form.Label>
+                                <Form.Control {...register('courseYear')} />
+                                <Form.Label>Numéro facture: </Form.Label>
+
+                                {getInvoiceNumber({
+                                    courseYear: courseYearWatched,
+                                    userCode: '1',
+                                    invoiceNumberForCurrentYear: selectedInvoiceData?.invoiceNumberForCurrentYear,
+                                })}
+                            </Col>
+                            <Col>
+                                <Form.Label>Date de facture</Form.Label>
+                                <Form.Control {...register('invoiceDate')} />
+                            </Col>
+                        </Row>
+                        <h6>Articles</h6>
+                        {selectedInvoiceData?.items?.map(({ designation, unit, amount, price }, index) => (
+                            <Row key={index}>
+                                <Col>
+                                    <Form.Label>Designation</Form.Label>
+                                    <Form.Control {...register(`items.${index}.designation`)} />
+                                </Col>
+                                <Col>
+                                    <Form.Label>Unité</Form.Label>
+                                    <Form.Control {...register(`items.${index}.unit`)} />
+                                </Col>
+                                <Col>
+                                    <Form.Label>Nombre</Form.Label>
+                                    <Form.Control {...register(`items.${index}.amount`)} />
+                                </Col>
+                                <Col>
+                                    <Form.Label>Prix</Form.Label>
+                                    <Form.Control {...register(`items.${index}.price`)} />
+                                </Col>
+                            </Row>
+                        ))}
+                        <h6>Autres</h6>
+                        <Row>
+                            <Col />
+                            <Col>
+                                <Form.Label>TVA</Form.Label>
+                                <Form.Control {...register('vatCode')} />
+                            </Col>
+                        </Row>
+                    </>
                 }
                 footer={
                     <>
