c0b7cd938628df3c2548529c0ee2abf017c076b0
Daniel Rodriguez <dani@plam.ch>
2022-08-14 19:28:15 +0300

    fix build

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,5 @@
 export const MIDDLEWARE_URL = process.env.REACT_APP_SERVICE_URL ?? 'http://localhost:4000'
 
-console.log(REACT_APP_SERVICE_URL, MIDDLEWARE_URL)
+console.log(process.env.REACT_APP_SERVICE_URL, MIDDLEWARE_URL)
 
 export const authCookiesMaxAgeSeconds = { true: 43200, false: 60 }
