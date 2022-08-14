68f58fa123d2cba4934a94d824812b80dd235042
Daniel Rodriguez <dani@plam.ch>
2022-08-14 19:22:50 +0300

    wip env vars

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,5 @@
 export const MIDDLEWARE_URL = process.env.REACT_APP_SERVICE_URL ?? 'http://localhost:4000'
 
+console.log(REACT_APP_SERVICE_URL, MIDDLEWARE_URL)
+
 export const authCookiesMaxAgeSeconds = { true: 43200, false: 60 }
