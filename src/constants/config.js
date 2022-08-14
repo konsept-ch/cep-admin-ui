e5ef5c381bb8f8febe6d5f62b5bd052db9b6ab90
Daniel Rodriguez <dani@plam.ch>
2022-08-14 15:33:25 +0300

    use env var for url when building

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,3 @@
-export const MIDDLEWARE_URL =
-    process.env.REACT_APP_MIDDLEWARE_URL ??
-    (process.env.NODE_ENV === 'production' ? 'https://middleware.cep-val.ch' : 'http://localhost:4000')
+export const MIDDLEWARE_URL = process.env.REACT_APP_SERVICE_URL ?? 'http://localhost:4000'
 
 export const authCookiesMaxAgeSeconds = { true: 43200, false: 60 }
