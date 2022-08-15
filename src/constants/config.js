87b85c95aaa01df94dd74d1ab81ffa1eae1ec71b
Daniel Rodriguez <dani@plam.ch>
2022-08-15 15:15:12 +0300

    cleanup

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,3 @@
-export const MIDDLEWARE_URL =
-    process.env.REACT_APP_SERVICES_URL ??
-    (process.env.NODE_ENV === 'production' ? 'http://localhost:4000' : 'http://localhost:4000')
-// (process.env.NODE_ENV === 'production' ? 'https://middleware.cep-val.ch' : 'http://localhost:4000')
+export const MIDDLEWARE_URL = process.env.REACT_APP_SERVICES_URL ?? 'http://localhost:4000'
 
 export const authCookiesMaxAgeSeconds = { true: 43200, false: 60 }
