1b0a9aaea58c8c9775476bfc257a26af754425ca
Daniel Rodriguez <dani@plam.ch>
2022-09-25 23:42:36 +0300

    suppress a console warning

--- src/saga/inscriptions.js
+++ src/saga/inscriptions.js
@@ -58,6 +58,7 @@ function* updateInscriptionsSaga(action) {
             }
         }
     } catch (error) {
+        // eslint-disable-next-line no-console
         console.error(error)
     } finally {
         yield put(setLoadingAction({ loading: false }))
