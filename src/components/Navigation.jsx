e0a8e3564dbde5d0e48f6711991b189b8daea5e4
Daniel Rodriguez <maieutiquer@gmail.com>
2023-02-06 00:52:01 +0200

    logout should redirect to home to workaround a bug

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -238,6 +238,7 @@ export const Navigation = ({ isLoggedIn }) => {
                     {isLoggedIn && (
                         <Nav>
                             <Nav.Link
+                                href="/"
                                 onClick={() => {
                                     clearAllAuthCookies()
                                 }}
