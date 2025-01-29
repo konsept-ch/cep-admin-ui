8161674c7560ca644725e540326fc90e2861fc97
firepolo <anthony.pasche@gmail.com>
2025-01-29 15:45:35 +0100

    remove saga

--- src/store.js
+++ src/store.js
@@ -1,15 +1,7 @@
-import createSagaMiddleware from 'redux-saga'
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query/react'
 
 import { rtkQueryErrorLogger } from './rtkQueryErrorLogger'
-import { rootSaga } from './saga'
-import { notificationsReducer } from './reducers/notifications'
-import { agendaReducer } from './reducers/agenda'
-import { inscriptionsReducer } from './reducers/inscriptions'
-import { parametersReducer } from './reducers/parameters'
-import { loadingReducer } from './reducers/loading'
-import { templatesReducer } from './reducers/templates'
 import { authApi } from './services/auth'
 import { usersApi } from './services/users'
 import { organizationsApi } from './services/organizations'
@@ -29,17 +21,8 @@ import { templatesApi } from './services/templates'
 import { tutorsApi } from './services/tutors'
 import { agendaApi } from './services/agenda'
 
-const sagaMiddleware = createSagaMiddleware()
-
 export const store = configureStore({
     reducer: {
-        notifications: notificationsReducer,
-        agenda: agendaReducer,
-        inscriptions: inscriptionsReducer,
-        parameters: parametersReducer,
-        loading: loadingReducer,
-        templates: templatesReducer,
-        // Add the generated reducer as a specific top-level slice
         [agendaApi.reducerPath]: agendaApi.reducer,
         [authApi.reducerPath]: authApi.reducer,
         [usersApi.reducerPath]: usersApi.reducer,
@@ -61,7 +44,6 @@ export const store = configureStore({
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat([
-            sagaMiddleware,
             agendaApi.middleware,
             authApi.middleware,
             usersApi.middleware,
@@ -83,8 +65,6 @@ export const store = configureStore({
             rtkQueryErrorLogger,
         ]),
 })
-
-sagaMiddleware.run(rootSaga)
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
 // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
