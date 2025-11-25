2a07744edc405c1e65fb731dcda900edc035c6b1
firepolo <anthony.pasche@gmail.com>
2025-11-25 11:30:52 +0100

    fix download contract

--- src/services/serviceUtils.js
+++ src/services/serviceUtils.js
@@ -33,13 +33,12 @@ export const prepareBaseQuery =
                 method,
                 body: body && !isFormData ? JSON.stringify(body) : body,
             })
+            if (options.responseHandler) return await options.responseHandler(response)
 
             const json = await response2Json(response)
 
             if (!response.ok) throw json
             if (json.message) toast[json.severity || 'success'](json.message)
-
-            //if (response.headers.has('metadata')) setStorage(JSON.parse(atob(response.headers.get('metadata'))))
 
             return { data: json }
         } catch (err) {
